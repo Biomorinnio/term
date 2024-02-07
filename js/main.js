9dd44a0ce55c8b7659715779bbcd678b76705a54
Biomorinnio <dkrysanov04@gmail.com>
2024-02-08 02:20:18 +0300

    aaa

--- js/main.js
+++ js/main.js
@@ -1,45 +1,56 @@
-const swiperLogos = new Swiper('.swiper-logos', {
-
-  slidesPerView: 7,
+const swiperLogos = new Swiper(".swiper-logos", {
+  slidesPerView: "auto",
   spaceBetween: 50,
 
   navigation: {
-    nextEl: '.swiper-button-next.logos',
-    prevEl: '.swiper-button-prev.logos',
+    nextEl: ".swiper-button-next.logos",
+    prevEl: ".swiper-button-prev.logos",
   },
+});
+const swiperCompanies = new Swiper(".swiper-companies", {
+  slidesPerView: 3,
+  spaceBetween: 22,
+});
+const swiperFeedback = new Swiper(".swiper-feedback", {
+  slidesPerView: 3,
+  spaceBetween: 40,
 
-  
-
+  navigation: {
+    nextEl: ".swiper-button-next.feedback",
+    prevEl: ".swiper-button-prev.feedback",
+  },
 });
 
 
-for(let i of  document.querySelectorAll('.phone-mask')){
-  
-  IMask(
-   i,
-    {
-      mask: '+{7}(000)000-00-00'
-    }
-  )
-}
+const acc = document?.getElementsByClassName("accordion");
 
-
-acc[0].classList.add('active');
-if(window.matchMedia("(max-width: 768px)").matches){
-  acc[0].nextElementSibling.style.maxHeight = acc[0].nextElementSibling.scrollHeight+20 + "px";
-}
-else{
-  acc[0].nextElementSibling.style.maxHeight = acc[0].nextElementSibling.scrollHeight + "px";
+acc[0].classList.add("active");
+if (window.matchMedia("(max-width: 768px)").matches) {
+  acc[0].nextElementSibling.style.maxHeight =
+    acc[0].nextElementSibling.scrollHeight + 20 + "px";
+} else {
+  acc[0].nextElementSibling.style.maxHeight =
+    acc[0].nextElementSibling.scrollHeight + "px";
 }
 
 for (let i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+  acc[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
-    if (panel.style.maxHeight){
+    if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
     } else {
       panel.style.maxHeight = panel.scrollHeight + "px";
     }
   });
 }
+
+
+
+
+for (let i of document.querySelectorAll(".phone-mask")) {
+  IMask(i, {
+    mask: "+{7}(000)000-00-00",
+  });
+}
+
